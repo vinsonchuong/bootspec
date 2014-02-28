a2a51c85e0eb2f77eb58e0dea93e66ca0d92a6ba
Eric Tsiliacos and Vinson Chuong <pair+etsiliacos+vchuong@pivotallabs.com>
2014-02-27 17:59:46 -0800

    Implement bootspec binary

--- lib/bootspec.rb
+++ lib/bootspec.rb
@@ -1,4 +1,11 @@
 require 'bootspec/version'
 
+require 'rspec'
+require 'rspec/core/formatters/documentation_formatter'
+require 'capybara/rspec'
+require 'capybara/poltergeist'
+Capybara.current_driver = :poltergeist
+Capybara.default_wait_time = 5
+
 module Bootspec
 end
